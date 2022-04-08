218f4e6df19f3758f8e14df0203a761eeaea382b
pawor <pawor.emi@gmail.com>
2022-04-08 21:39:47 +0700

    create models for api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(console.log("Connected to MONGODB")).catch(err => console.log(err))
 
 app.listen(5000, () => {
     console.log("Backend is running.")
