8ef02ac7cab29f14383d2e5fa284094023fbc5b5
pawor <pawor.emi@gmail.com>
2022-04-09 22:36:48 +0700

    Design Login and Register pages

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -1,27 +1,53 @@
 import './topbar.css'
+import { Link } from 'react-router-dom'
 
 export default function TopBar() {
-  return (
-    <div className='top'>
-        <div className='topLeft'>
-            <i className="topIcon fa-brands fa-facebook-square"></i>
-            <i className="topIcon fa-brands fa-twitter-square"></i>
-            <i className="topIcon fa-brands fa-pinterest-square"></i>
-            <i className="topIcon fa-brands fa-instagram-square"></i>
-        </div>
-        <div className='topCenter'>
-            <ul className='topList'>
-                <li className='topListItem'>Home</li>
-                <li className='topListItem'>ABOUT</li>
-                <li className='topListItem'>CONTACT</li>
-                <li className='topListItem'>WRITE</li>
-                <li className='topListItem'>LOGOUT</li>
-            </ul>
-        </div>
-        <div className='topRight'>
-            <img className='topImg' src='/images/8239.jpg' alt=''/>
-            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-        </div>
+    const user = false
+
+    return (
+        <div className='top'>
+            <div className='topLeft'>
+                <i className="topIcon fa-brands fa-facebook-square"></i>
+                <i className="topIcon fa-brands fa-twitter-square"></i>
+                <i className="topIcon fa-brands fa-pinterest-square"></i>
+                <i className="topIcon fa-brands fa-instagram-square"></i>
+            </div>
+
+            <div className='topCenter'>
+                <ul className='topList'>
+                    <li className='topListItem'>
+                        <Link className='link' to="/">HOME</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link className='link' to="/">ABOUT</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link className='link' to="/">CONTACT</Link>
+                    </li>
+                    <li className='topListItem'>
+                        <Link className='link' to="/write">WRITE</Link>
+                    </li>
+                    <li className='topListItem'>
+                        {user && "LOGOUT"}
+                    </li>
+                </ul>
+            </div>
+
+            <div className='topRight'>
+                {user ? (
+                    <img className='topImg' src='/images/8239.jpg' alt=''/>
+                    ):(
+                    <ul className='topList'>
+                        <li className='topListItem'>
+                            <Link className='link' to="/login">LOGIN</Link>
+                        </li>
+                        <li className='topListItem'>
+                            <Link className='link' to="/register">REGISTER</Link>
+                        </li>
+                    </ul>)
+                }
+                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+            </div>
     </div>
   )
 }
--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -5,6 +5,27 @@ export default function Setting() {
   return (
     <div className='settings'>
       <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className='settingsUpdateTitle'>Update Your Account</span>
+          <span className='settingsDeleteTitle'>Delete Account</span>
+        </div>
+        <form className='settingsForm'>
+          <label>Profile Picture</label>
+          <div className='settingsPP'>
+            <img src='/images/8239.jpg' alt=''/>
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-solid fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{display:"none"}}/>
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder='Pawornrat'/>
+          <label>Email</label>
+          <input type="email" placeholder='pawornrat.2680@gmail.com'/>
+          <label>Password</label>
+          <input type="password"/>
+          <button className='settingsSubmit'>Update</button>
+        </form>
       </div>
       <Sidebar />
     </div>
