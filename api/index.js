d4cd9d09d59d53e50e439e4df0111e20ba65fc3a
pawor <pawor.emi@gmail.com>
2022-04-11 17:16:12 +0700

    Rest API for backend

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
+const catRoute = require('./routes/categories')
+const multer = require('multer')
 const app = express()
 
 app.use(express.json())
@@ -15,9 +17,24 @@ mongoose.connect(process.env.MONGODB_URL, {
     useCreateIndex: true
 }).then(console.log("Connected to MONGODB")).catch(err => console.log(err))
 
+const storage = multer.diskStorage({
+    destination : (req, file, cb) => {
+        cb(null, "images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage: storage})
+app.post('/api/upload', upload.single("file"), (req,res) => {
+    res.status(200).json("File has been uploaded")
+})
+
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/posts', postRoute)
+app.use('/api/categories', catRoute)
 
 app.listen(5000, () => {
     console.log("Backend is running.")
