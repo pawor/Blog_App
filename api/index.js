6dbaa0f6bd6c021c9c92569341d6dc6e9f269f49
pawor <pawor.emi@gmail.com>
2022-04-11 15:33:20 +0700

    create CRUD for Post backend

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
+const postRoute = require('./routes/posts')
 const app = express()
 
 app.use(express.json())
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
 
 app.listen(5000, () => {
     console.log("Backend is running.")
